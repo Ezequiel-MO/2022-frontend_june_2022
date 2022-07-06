9958581caf1f8ea2b108d2e69843352b94ad8017
Ezequiel <oliverm316@gmail.com>
2022-07-06 14:26:46 +0200

    minor fix

--- src/components/hotels/Hotels.jsx
+++ src/components/hotels/Hotels.jsx
@@ -7,17 +7,20 @@ const Hotels = () => {
   const [openTab, setOpenTab] = useState(1);
   const { currentProject } = useCurrentProject();
   const { hotels } = currentProject;
+  const [hotelsState] = useState(hotels);
+
+  console.log(hotelsState);
 
   return (
     <>
       <div className="flex flex-wrap" id="hotels_id">
-        {hotels && hotels.length > 0 ? (
+        {hotelsState?.length > 0 ? (
           <div className="w-full">
             <ul
               className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
               role="tablist"
             >
-              {hotels?.map((hotel, index) => (
+              {hotelsState?.map((hotel, index) => (
                 <li key={hotel._id} className="mr-2 last:mr-0 flex-auto">
                   <a
                     className="text-sm font-bold uppercase px-5 py-3 shadow-sm rounded block leading-normal bg-white-100 hover:bg-gray-100 dark:bg-gray-50 dark:hover:bg-green-50 dark:hover:text-black-50 focus:outline-none focus:shadow-outline"
@@ -36,7 +39,7 @@ const Hotels = () => {
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 rounded">
               <div className="px-4 py-5 flex-auto">
                 <div className="tab-content tab-space">
-                  {hotels.map((hotel, index) => (
+                  {hotelsState.map((hotel, index) => (
                     <div
                       key={hotel._id}
                       className={openTab === index + 1 ? "block" : "hidden"}
