98dfab743cd5ec4b352b177e5a5556e1ece24be3
Atul Choudhary <t-atul1.choudhary@paytmmoney.com>
2022-11-02 20:11:23 +0530

    Auto detection dark and light mode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,27 +1,63 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 const useDarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState(
-    () => localStorage.theme === 'dark'
-  )
+    () => localStorage.theme === "dark"
+  );
 
   const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode)
-  }
+    setIsDarkMode(!isDarkMode);
+  };
+  useEffect(() => {
+    // Add listener to update styles
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) =>
+        onSelectMode(e.matches ? "dark" : "light")
+      );
+
+    // Setup dark/light mode for the first time
+    onSelectMode(
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light"
+    );
+
+    // Remove listener
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", () => {});
+    };
+  }, []);
+
+  const onSelectMode = (mode) => {
+    const html = window.document.documentElement;
+
+    if (mode === "dark") {
+      html.classList.remove("light");
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+      html.classList.add("light");
+    }
+
+    localStorage.setItem("theme", mode);
+  };
 
   useEffect(() => {
-    const html = window.document.documentElement
+    const html = window.document.documentElement;
 
-    const prevTheme = isDarkMode ? 'light' : 'dark'
-    html.classList.remove(prevTheme)
+    const prevTheme = isDarkMode ? "light" : "dark";
+    html.classList.remove(prevTheme);
 
-    const nextTheme = isDarkMode ? 'dark' : 'light'
-    html.classList.add(nextTheme)
+    const nextTheme = isDarkMode ? "dark" : "light";
+    html.classList.add(nextTheme);
 
-    localStorage.setItem('theme', nextTheme)
-  }, [isDarkMode])
+    localStorage.setItem("theme", nextTheme);
+  }, [isDarkMode]);
 
-  return [isDarkMode, toggleDarkMode]
-}
+  return [isDarkMode, toggleDarkMode];
+};
 
-export default useDarkMode
+export default useDarkMode;
