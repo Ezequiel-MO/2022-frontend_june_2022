c6fa05aa763bdf1cbfd62a50627cec8937ebe8ac
Atul Choudhary <t-atul1.choudhary@paytmmoney.com>
2022-11-03 23:31:22 +0530

    #7 issue resolved

--- src/components/budget/BudgetTableHead.jsx
+++ src/components/budget/BudgetTableHead.jsx
@@ -7,31 +7,31 @@ const BudgetTableHead = () => {
         <TableCell width="10%" />
         <TableCell
           width="20%"
-          className="dark:text-orange-50 text-orange-50 dark:font-extrabold "
+          className="dark:text-orange-50 text-orange-50 dark:font-extrabold uppercase"
         >
           Event Type
         </TableCell>
         <TableCell
           width="35%"
-          className="dark:text-orange-50 text-orange-50 dark:font-extrabold "
+          className="dark:text-orange-50 text-orange-50 dark:font-extrabold uppercase"
         >
           Service
         </TableCell>
         <TableCell
           width="5%"
-          className="dark:text-orange-50 text-orange-50 dark:font-extrabold "
+          className="dark:text-orange-50 text-orange-50 dark:font-extrabold uppercase"
         >
           Pax/units
         </TableCell>
         <TableCell
           width="15%"
-          className="dark:text-orange-50 text-orange-50 dark:font-extrabold "
+          className="dark:text-orange-50 text-orange-50 dark:font-extrabold uppercase"
         >
           Unit cost w/VAT
         </TableCell>
         <TableCell
           width="15%"
-          className="dark:text-orange-50 text-orange-50 dark:font-extrabold "
+          className="dark:text-orange-50 text-orange-50 dark:font-extrabold uppercase"
         >
           Total cost w/VAT
         </TableCell>
