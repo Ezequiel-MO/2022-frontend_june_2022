4cd7710679449896509ff3b6cabcef72288ef77d
Ezequiel <oliverm316@gmail.com>
2022-11-05 00:03:03 +0100

    minor restyling

--- src/components/sidebar/SidebarRow.jsx
+++ src/components/sidebar/SidebarRow.jsx
@@ -16,7 +16,7 @@ function SidebarRow({ iconText, title, modal = false, handleOpen }) {
         data-for='main'
         data-tip={title}
         data-iscapture='true'
-        className='flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-green-50 cursor-pointer transition-all duration-200 group'
+        className='flex items-center space-x-2 px-4 py-3 rounded-lg hover:bg-green-50 cursor-pointer transition-all duration-200 group'
         onClick={() => handleOpen(`${title}`)}
       >
         <Icon icon={iconText} color='#ea5933' width='40' />
