c469a2d23c437f90f19852d11756b9a8b0b0db08
Ezequiel <oliverm316@gmail.com>
2023-03-02 14:43:24 +0100

    MapLogic to load locations of our main destinations

--- src/components/vendor_map/MapLogic.js
+++ src/components/vendor_map/MapLogic.js
@@ -33,19 +33,27 @@ export const VendorMapLogic = () => {
   const { currentProject } = useCurrentProject()
   const { hotels, schedule, groupLocation } = currentProject
 
+  const locations = {
+    Barcelona: [41.385331792, 2.168665992],
+    Madrid: [40.4167754, -3.7037902],
+    Valencia: [39.4699075, -0.3762881],
+    Sevilla: [37.3890924, -5.9844589],
+    'Costa Brava': [41.992046, 3.156307],
+    Andorra: [42.506285, 1.521801],
+    Lisboa: [38.7222524, -9.1393366],
+    Bilbao: [43.2630136, -2.9349852],
+    Mallorca: [39.695263, 3.017571],
+    Alicante: [38.345996, -0.490685]
+  }
+
   const centralCoords = useMemo(() => {
-    let locationCoords =
-      groupLocation === 'Barcelona'
-        ? [41.385331792, 2.168665992]
-        : groupLocation === 'Alicante'
-        ? [38.345996, -0.490685]
-        : [40.4167754, -3.7037902]
+    const coords = locations[groupLocation] || locations.Barcelona
     return {
       place: groupLocation,
       icon: city_icon,
       coords: {
-        lat: locationCoords[0],
-        lng: locationCoords[1]
+        lat: coords[0],
+        lng: coords[1]
       }
     }
   }, [groupLocation])
