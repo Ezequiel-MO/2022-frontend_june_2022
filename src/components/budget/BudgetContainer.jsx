d112a8e9232c80f7cc78806fe311723e1d400a30
Ezequiel <oliverm316@gmail.com>
2023-03-23 14:44:59 +0100

    minor fix

--- src/components/budget/BudgetContainer.jsx
+++ src/components/budget/BudgetContainer.jsx
@@ -2,7 +2,7 @@ import { forwardRef } from 'react'
 
 export const BudgetContainer = forwardRef(({ children }, ref) => (
   <div
-    className='no-scrollbar overflow-x-auto relative lg:-mx-40 xl:-mx-0 z-20 bg-[#fff] mb-10'
+    className='overflow-x-auto relative -mx-56 2xl:-mx-0 z-20 bg-[#fff] mb-10'
     id='budget_id'
     ref={ref}
   >
