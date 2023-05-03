3978bac0b3fe9d1543a3d8e7952e94eeb1dcddab
Ezequiel <oliverm316@gmail.com>
2023-05-03 15:54:09 +0200

    pdf rendering and printing enhanced

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -11,97 +11,118 @@ import { RichParagraph } from './atoms/RichParagraph'
 import { Document, Page } from 'react-pdf/dist/esm/entry.vite'
 import { Budget } from './budget/MainTable/higherComponents'
 import { PartialCosts } from './budget/partial-costs/'
-import io from 'socket.io-client'
 
-const MainSection = forwardRef(({ setIconColor, onReady }, ref) => {
-  const componentRef = useRef()
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber] = useState(1)
-  const { currentProject } = useCurrentProject()
-  const {
-    groupName,
-    projectIntro,
-    hotels,
-    clientCompany,
-    budget,
-    imageContentUrl
-  } = currentProject
-  const { t } = useTranslation()
-  const { fonts = [], colorPalette = [] } = clientCompany[0] || {}
+const MainSection = forwardRef(
+  ({ setIconColor, onReady, parentWidth }, ref) => {
+    const componentRef = useRef()
+    const [numPages, setNumPages] = useState(null)
+    const [pageNumber] = useState(1)
+    const { currentProject } = useCurrentProject()
+    const {
+      groupName,
+      projectIntro,
+      hotels,
+      clientCompany,
+      budget,
+      imageContentUrl
+    } = currentProject
+    const { t } = useTranslation()
+    const { fonts = [], colorPalette = [] } = clientCompany[0] || {}
 
-  const iconColor = colorPalette.length > 0 ? colorPalette[2] : '#ea5933'
+    const iconColor = colorPalette.length > 0 ? colorPalette[2] : '#ea5933'
 
-  const fontFamilyStyle = useFontFamily(fonts[0])
+    const fontFamilyStyle = useFontFamily(fonts[0])
+    const pdfToPrintRef = useRef()
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages)
-  }
-
-  useEffect(() => {
-    if (onReady) {
-      onReady(true)
+    function onDocumentLoadSuccess({ numPages }) {
+      setNumPages(numPages)
     }
-  }, [])
 
-  useEffect(() => {
-    setIconColor(iconColor)
-  }, [iconColor])
+    useEffect(() => {
+      if (onReady) {
+        onReady(true)
+      }
+    }, [])
 
-  /* useEffect(() => {
+    useEffect(() => {
+      setIconColor(iconColor)
+    }, [iconColor])
+
+    /* useEffect(() => {
     let socket = io(import.meta.env.VITE_BACKEND_URL)
     console.log('socket', socket)
   }, []) */
 
-  return (
-    <div
-      ref={ref}
-      className={`${fontFamilyStyle} col-span-10 lg:col-span-8 z-50 bg-white-0 dark:bg-black-50`}
-    >
-      <h1 className='text-2xl md:text-2xl mb-4 font-extrabold'>
-        {`${t('quotation')} Gr. ${groupName}`}
-      </h1>
-      <RichParagraph text={projectIntro} />
-      <Hotels hotels={hotels} />
-      <Schedule />
-      {budget === 'budget' ? (
-        <div>
-          <ReactToPrint
-            trigger={() => (
-              <button className='flex flex-row items-center mb-2 py-4 printable-icon'>
-                <span>
-                  <Icon
-                    icon='ant-design:file-pdf-twotone'
-                    color={`${
-                      colorPalette.length > 0 ? colorPalette[2] : '#ea5933'
-                    }`}
-                    width='40'
-                  />
-                </span>
-                {t('budgetPrint')}
-              </button>
-            )}
-            content={() => componentRef.current}
-          />
-          <Budget ref={componentRef} />
-          <PartialCosts colorPalette={colorPalette} />
-        </div>
-      ) : budget === 'budgetAsPdf' ? (
-        <div>
-          <Document
-            file={imageContentUrl[0]}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
-      ) : null}
+    return (
+      <div
+        ref={ref}
+        className={`${fontFamilyStyle} col-span-10 lg:col-span-8 z-50 bg-white-0 dark:bg-black-50`}
+      >
+        <h1 className='text-2xl md:text-2xl mb-4 font-extrabold'>
+          {`${t('quotation')} Gr. ${groupName}`}
+        </h1>
+        <RichParagraph text={projectIntro} />
+        <Hotels hotels={hotels} />
+        <Schedule />
+        {budget === 'budget' ? (
+          <div>
+            <ReactToPrint
+              trigger={() => (
+                <button className='flex flex-row items-center mb-2 py-4 printable-icon'>
+                  <span>
+                    <Icon
+                      icon='ant-design:file-pdf-twotone'
+                      color={`${
+                        colorPalette.length > 0 ? colorPalette[2] : '#ea5933'
+                      }`}
+                      width='40'
+                    />
+                  </span>
+                  {t('budgetPrint')}
+                </button>
+              )}
+              content={() => componentRef.current}
+            />
+            <Budget ref={componentRef} />
+            <PartialCosts colorPalette={colorPalette} />
+          </div>
+        ) : budget === 'budgetAsPdf' ? (
+          <div className='page-break-after'>
+            <ReactToPrint
+              trigger={() => (
+                <button className='flex flex-row items-center mb-2 py-4 printable-icon'>
+                  <span>
+                    <Icon
+                      icon='ant-design:file-pdf-twotone'
+                      color={`${
+                        colorPalette.length > 0 ? colorPalette[2] : '#ea5933'
+                      }`}
+                      width='40'
+                    />
+                  </span>
+                  {t('budgetPrint')}
+                </button>
+              )}
+              content={() => pdfToPrintRef.current}
+            />
+            <div ref={pdfToPrintRef}>
+              <Document
+                file={imageContentUrl[0]}
+                onLoadSuccess={onDocumentLoadSuccess}
+              >
+                <Page pageNumber={pageNumber} width={parentWidth} />
+              </Document>
+              <p className='page-number'>
+                Page {pageNumber} of {numPages}
+              </p>
+            </div>
+          </div>
+        ) : null}
 
-      <ScrollToTopButton iconColor={iconColor} />
-    </div>
-  )
-})
+        <ScrollToTopButton iconColor={iconColor} />
+      </div>
+    )
+  }
+)
 
 export default MainSection
--- src/screens/MainPage.jsx
+++ src/screens/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import { Footer, MainSection, Sidebar, SidebarSmall } from '../components'
 import { useCurrentProject } from '../hooks'
 import { TranslationProvider } from '../translations/translationContext'
@@ -10,6 +10,15 @@ export const MainPage = () => {
   const { hasSideMenu, clientAccManager = [] } = currentProject
   const { quoteLanguage = 'EN' } = clientAccManager[0] || {}
   const mainSectionRef = useRef()
+  const [parentWidth, setParentWidth] = useState(0)
+  const mainSectionParentRef = useRef()
+
+  useLayoutEffect(() => {
+    if (mainSectionParentRef && mainSectionParentRef.current) {
+      setParentWidth(mainSectionParentRef.current.clientWidth)
+    }
+  }, [mainSectionParentRef])
+
   return (
     <TranslationProvider quoteLanguage={quoteLanguage}>
       <div className='flex flex-col'>
@@ -21,11 +30,15 @@ export const MainPage = () => {
 
         <div className='grid grid-cols-12 m-8'>
           {hasSideMenu ? <Sidebar /> : <div className='col-span-2' />}
-          <div className='relative col-span-10 lg:col-span-8'>
+          <div
+            ref={mainSectionParentRef}
+            className='relative col-span-10 lg:col-span-8'
+          >
             <MainSection
               ref={mainSectionRef}
               setIconColor={setIconColor}
               onReady={setIsMainSectionReady}
+              parentWidth={parentWidth}
             />
           </div>
         </div>
