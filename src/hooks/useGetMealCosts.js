0cbce2944a4a63e212b6511fa009b3dd8a18e532
Ezequiel <oliverm316@gmail.com>
2023-05-05 17:47:08 +0200

    Bug fixed at useGetMealCosts

--- src/hooks/useGetMealCosts.js
+++ src/hooks/useGetMealCosts.js
@@ -4,15 +4,15 @@ import { useBudget } from './useBudget'
 export const useGetMealsCost = () => {
   const { meals } = useBudget()
   const [mealsTotalCost, setMealsTotalCost] = useState(0)
-
   useEffect(() => {
     const mealsArray = Object.values(meals)
-    const newArray = mealsArray.map((meal) => {
-      return Object.values(meal).map(({ totalCost }) => {
-        return totalCost
-      })
+    let totalCostPerDayArray = mealsArray.map((day) => {
+      return Object.values(day)
+        .map((meal) => meal.price)
+        .reduce((a, b) => a + b, 0)
     })
-    const totalCost = newArray.flat().reduce((a, b) => a + b, 0)
+
+    let totalCost = totalCostPerDayArray.reduce((a, b) => a + b, 0)
 
     setMealsTotalCost(totalCost)
   }, [meals])
