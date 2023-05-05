c18251cd1074c56982b661968b05eb8682dd6509
Ezequiel <oliverm316@gmail.com>
2023-05-05 11:33:39 +0200

    TabItem restyling

--- src/components/atoms/TabItem.jsx
+++ src/components/atoms/TabItem.jsx
@@ -2,9 +2,9 @@ export const TabItem = ({ tabListItem, index, activeTab, setActiveTab }) => {
   const isActive = activeTab === index + 1
 
   return (
-    <li className='mr-2 last:mr-0 flex-auto' id={tabListItem._id}>
+    <li className='m-1 last:mr-0' id={tabListItem._id}>
       <a
-        className={`text-sm font-bold uppercase px-5 py-3 shadow-sm rounded block leading-normal ${
+        className={`max-w-md text-sm font-bold uppercase px-5 py-3 shadow-sm rounded block leading-normal ${
           isActive
             ? 'dark:bg-orange-50 bg-orange-50 text-white-100 transition-all ease-in-out'
             : 'bg-white-100 dark:bg-gray-50 transition-all ease-in-out'
