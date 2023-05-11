e1a489f5d00e9d883a626acba90fbb87a1bf223b
Ezequiel <oliverm316@gmail.com>
2023-05-11 11:04:42 +0200

    interceptor added to api requests in axios config

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -2,9 +2,19 @@ import axios from 'axios'
 
 const baseAPI = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}`
-  /* headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_AUTH_TOKEN}`,
-  }, */
 })
+
+// Add a request interceptor
+baseAPI.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token') || null
+    config.headers.Authorization = token ? `Bearer ${token}` : ''
+    return config
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error)
+  }
+)
 
 export default baseAPI
