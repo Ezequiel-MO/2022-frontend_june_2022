86d6a42c968cf6a8f7d5b2538cb86696811c0ea9
Ezequiel <oliverm316@gmail.com>
2023-05-11 11:45:07 +0200

    FIX

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -4,17 +4,4 @@ const baseAPI = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}`
 })
 
-// Add a request interceptor
-baseAPI.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token') || null
-    config.headers.Authorization = token ? `Bearer ${token}` : ''
-    return config
-  },
-  (error) => {
-    // Do something with request error
-    return Promise.reject(error)
-  }
-)
-
 export default baseAPI
