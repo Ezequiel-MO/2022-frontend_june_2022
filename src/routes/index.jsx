d2642b1d0e8a70e8b286d92c0652f9fbc00ecf22
Ezequiel <oliverm316@gmail.com>
2023-05-11 13:49:43 +0200

    index routes changed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,30 +1,44 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements
-} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import LoginPage from '../components/login/LoginPage'
-import { AuthLayout, ProtectedRoute } from '../layouts'
+import { AuthLayout } from '../layouts'
 import { Credentials, Destination, MainPage } from '../screens'
 import NotFound from '../ui/NotFound'
 import { ErrorPage } from './error-page'
+import GeneralLayout from '../layouts/GeneralLayout'
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path='/' element={<AuthLayout />}>
-        <Route index element={<LoginPage />} />
-      </Route>
-      <Route
-        path='/app'
-        element={<ProtectedRoute />}
-        errorElement={<ErrorPage />}
-      >
-        <Route index element={<MainPage />} />
-        <Route path='credentials' element={<Credentials />} />
-        <Route path='destination_info' element={<Destination />} />
-      </Route>
-      <Route path='*' element={<NotFound />} />
-    </Route>
-  )
-)
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />
+      }
+    ]
+  },
+  {
+    path: '/app',
+    element: <GeneralLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />
+      },
+      {
+        path: 'credentials',
+        element: <Credentials />
+      },
+      {
+        path: 'destination_info',
+        element: <Destination />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+])
