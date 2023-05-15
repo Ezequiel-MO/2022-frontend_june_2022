8d0d69c597ae06c4bc596484384abaf9b181522e
Ezequiel <oliverm316@gmail.com>
2023-05-15 10:40:38 +0200

    headers added to baseAPI

--- src/axios/axiosConfig.js
+++ src/axios/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const baseAPI = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}`
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  }
 })
 
 export default baseAPI
