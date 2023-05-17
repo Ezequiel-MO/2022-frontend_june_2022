386cbb8338305a62eab70a06f76e44ece686eb62
Ezequiel <oliverm316@gmail.com>
2023-05-17 18:26:05 +0200

    test for useGetTransferCosts passing

--- src/hooks/__tests__/useGetTransferCosts.test.js
+++ src/hooks/__tests__/useGetTransferCosts.test.js
@@ -1,15 +1,21 @@
 import { renderHook } from '@testing-library/react'
 import { useGetTransferCosts } from '../useGetTransferCosts'
 import { useBudget } from '../useBudget'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import budgetReducer from '../../redux/features/budgetSlice'
 
 jest.mock('../useBudget')
 
 describe('useGetTransferCosts hook', () => {
+  let store
   beforeEach(() => {
-    jest.clearAllMocks()
+    store = configureStore({
+      reducer: { budget: budgetReducer }
+    })
   })
 
-  test('calculates the total transfer costs correctly', () => {
+  it('calculates the total transfer costs correctly', () => {
     useBudget.mockReturnValue({
       transfers: {
         transfer1: 100,
@@ -31,11 +37,13 @@ describe('useGetTransferCosts hook', () => {
       }
     })
 
-    const { result } = renderHook(() => useGetTransferCosts())
+    const { result } = renderHook(() => useGetTransferCosts(), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
+    })
     expect(result.current.transfersTotalCost).toEqual(1270)
   })
 
-  test('handles missing values gracefully', () => {
+  it('handles missing values gracefully', () => {
     useBudget.mockReturnValue({
       transfers: {
         transfer1: 100,
@@ -49,18 +57,57 @@ describe('useGetTransferCosts hook', () => {
       transfersOut: {}
     })
 
-    const { result } = renderHook(() => useGetTransferCosts())
+    const { result } = renderHook(() => useGetTransferCosts(), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
+    })
     expect(result.current.transfersTotalCost).toEqual(600)
   })
 
-  test('returns 0 when there are no transfers', () => {
+  it('returns 0 when there are no transfers', () => {
     useBudget.mockReturnValue({
       transfers: {},
       transfersIn: {},
       transfersOut: {}
     })
 
-    const { result } = renderHook(() => useGetTransferCosts())
+    const { result } = renderHook(() => useGetTransferCosts(), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
+    })
     expect(result.current.transfersTotalCost).toEqual(0)
+  })
+  it('updates total transfers cost when transfers are updated', () => {
+    useBudget.mockReturnValue({
+      transfers: {
+        transfer1: 100,
+        transfer2: 200,
+        transfer3: 300
+      },
+      transfersIn: {
+        assistance: 2,
+        meetGreet: 3
+      },
+      transfersOut: {}
+    })
+
+    const { result, rerender } = renderHook(() => useGetTransferCosts(), {
+      wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
+    })
+    expect(result.current.transfersTotalCost).toEqual(600)
+
+    useBudget.mockReturnValue({
+      transfers: {
+        transfer1: 100,
+        transfer2: 200,
+        transfer3: 300,
+        transfer4: 400
+      },
+      transfersIn: {
+        assistance: 2,
+        meetGreet: 3
+      },
+      transfersOut: {}
+    })
+    rerender()
+    expect(result.current.transfersTotalCost).toEqual(1000)
   })
 })
