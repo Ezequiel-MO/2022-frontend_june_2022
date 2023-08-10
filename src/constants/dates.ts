466aa682937da7a5efe3b340d206a77916229dec
Ezequiel <oliverm316@gmail.com>
2023-08-10 14:29:28 +0200

    Overview back in place

---
+++ src/constants/dates.ts
@@ -0,0 +1,24 @@
+export const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+
+export const daysOfTheWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
