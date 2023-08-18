f32f68e8f800a055667ce5a2371f2783a3c96150
Ezequiel <oliverm316@gmail.com>
2023-08-18 11:14:20 +0200

    Hotel Breakdown Rows down and up smooth transition

--- src/components/budget/MainTable/hotel/HotelBreakdownRows.tsx
+++ src/components/budget/MainTable/hotel/HotelBreakdownRows.tsx
@@ -45,54 +45,66 @@ export const HotelBreakdownRows = ({ hotels, nights }: Props) => {
           </button>
         </td>
       </tr>
-      {isOpen && (
-        <tr className='w-full bg-white-100 dark:bg-[#a9ba9d] relative'>
-          <div className='absolute inset-0 flex items-center justify-center opacity-10 dark:opacity-20 z-0'>
-            <Icon icon='ic:twotone-local-hotel' width={300} />
-          </div>
-          <td colSpan={6} className='p-0 bg-transparent'>
+      <tr>
+        <td colSpan={6} className='p-0 bg-transparent'>
+          <div
+            className={`transition-all duration-500 ease-in-out overflow-hidden ${
+              isOpen ? 'max-h-[800px] opacity-100' : 'max-h-0 opacity-0'
+            }`}
+          >
             <table className='w-full'>
-              <thead className='text-white-100 bg-zinc-800'>
+              <tbody className='w-full bg-white-100 dark:bg-[#a9ba9d] relative'>
                 <tr>
-                  <td align='center'>Description</td>
-                  <td align='center'>Nr. Units </td>
-                  <td align='center'>Nr. of nights </td>
-                  <td align='center'>Cost per room per night</td>
-                  <td align='center'>Total Cost</td>
+                  <td colSpan={6} className='p-0 bg-transparent'>
+                    <div className='absolute inset-0 flex items-center justify-center opacity-10 dark:opacity-20 z-0'>
+                      <Icon icon='ic:twotone-local-hotel' width={300} />
+                    </div>
+                    <table className='w-full'>
+                      <thead className='text-white-100 bg-zinc-800'>
+                        <tr>
+                          <td align='center'>Description</td>
+                          <td align='center'>Nr. Units </td>
+                          <td align='center'>Nr. of nights </td>
+                          <td align='center'>Cost per room per night</td>
+                          <td align='center'>Total Cost</td>
+                        </tr>
+                      </thead>
+                      <tbody className='text-[#000]'>
+                        <HotelBreakdownRow
+                          units={DUInr}
+                          rate={DUIprice}
+                          nights={nights}
+                          title='Double Room Single Use'
+                        />
+                        <HotelBreakdownRow
+                          units={DoubleRoomNr}
+                          rate={DoubleRoomPrice}
+                          nights={nights}
+                          title='Double Room //Twin Room'
+                        />
+                        <HotelBreakdownRow
+                          units={DUInr + DoubleRoomNr * 2}
+                          rate={DailyTax}
+                          nights={nights}
+                          title='City Tax'
+                        />
+                        {breakfast ? (
+                          <HotelBreakdownRow
+                            units={DUInr + DoubleRoomNr * 2}
+                            rate={breakfast}
+                            nights={nights}
+                            title='Breakfast'
+                          />
+                        ) : null}
+                      </tbody>
+                    </table>
+                  </td>
                 </tr>
-              </thead>
-              <tbody className='dark:text-[#000]'>
-                <HotelBreakdownRow
-                  units={DUInr}
-                  rate={DUIprice}
-                  nights={nights}
-                  title='Double Room Single Use'
-                />
-                <HotelBreakdownRow
-                  units={DoubleRoomNr}
-                  rate={DoubleRoomPrice}
-                  nights={nights}
-                  title='Double Room //Twin Room'
-                />
-                <HotelBreakdownRow
-                  units={DUInr + DoubleRoomNr * 2}
-                  rate={DailyTax}
-                  nights={nights}
-                  title='City Tax'
-                />
-                {breakfast ? (
-                  <HotelBreakdownRow
-                    units={DUInr + DoubleRoomNr * 2}
-                    rate={breakfast}
-                    nights={nights}
-                    title='Breakfast'
-                  />
-                ) : null}
               </tbody>
             </table>
-          </td>
-        </tr>
-      )}
+          </div>
+        </td>
+      </tr>
     </>
   )
 }
