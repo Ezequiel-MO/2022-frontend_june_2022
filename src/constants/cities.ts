7cd3bd156aca2b5112f34cb4ae106b2dcd326281
Ezequiel <oliverm316@gmail.com>
2023-08-29 17:30:12 +0200

    MapLogic converted to typescript

--- src/constants/cities.ts
+++ src/constants/cities.ts
@@ -1,12 +1,27 @@
-export const cities: string[] = [
+type CitiesType =
+  | 'Barcelona'
+  | 'Paris'
+  | 'Bilbao'
+  | 'Valencia'
+  | 'Madrid'
+  | 'Gran Canaria'
+  | 'San Sebastian'
+
+export const cities: CitiesType[] = [
   'Barcelona',
   'Paris',
   'Bilbao',
   'Valencia',
-  'Madrid'
+  'Madrid',
+  'Gran Canaria',
+  'San Sebastian'
 ]
 
-export const locations = {
+export interface IMapLocations {
+  [key: string]: [number, number]
+}
+
+export const locations: IMapLocations = {
   Barcelona: [41.385331792, 2.168665992],
   Madrid: [40.4167754, -3.7037902],
   Valencia: [39.4699075, -0.3762881],
@@ -17,5 +32,7 @@ export const locations = {
   Bilbao: [43.2630136, -2.9349852],
   Mallorca: [39.695263, 3.017571],
   Alicante: [38.345996, -0.490685],
-  Paris: [48.8566969, 2.3514616]
+  Paris: [48.8566969, 2.3514616],
+  'Gran Canaria': [27.9202209, -15.5474374],
+  'San Sebastian': [43.318334, -1.981231]
 }
