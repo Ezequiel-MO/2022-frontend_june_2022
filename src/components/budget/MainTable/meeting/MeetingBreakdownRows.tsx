bfed5a4a3084b04ee1b5a8e60f96dd28e4725b7c
Ezequiel <oliverm316@gmail.com>
2023-11-03 13:29:43 +0100

    Partial costs for shows up

--- src/components/budget/MainTable/meeting/MeetingBreakdownRows.tsx
+++ src/components/budget/MainTable/meeting/MeetingBreakdownRows.tsx
@@ -21,100 +21,87 @@ export const MeetingBreakdownRows = ({
   isOpen
 }: Props) => {
   const { hotelName } = useBudget()
-
   const { meeting } = useFindMeetingByHotel(meetings, hotelName)
 
   if (!meeting) return null
 
   return (
-    <>
-      <tr
-        style={{
-          transition: 'all 0.5s ease-in-out',
-          maxHeight: isOpen ? '800px' : '0',
-          overflow: 'hidden',
-          opacity: isOpen ? '1' : '0'
-        }}
-      >
-        <td colSpan={6} className='p-0 bg-transparent'>
-          <div
-            className={`transition-all duration-500 ease-in-out overflow-hidden ${
-              isOpen ? 'max-h-[800px] opacity-100' : 'max-h-0 opacity-0'
-            }`}
-          >
+    <tr
+      className={`transition-all duration-500 ease-in-out ${
+        isOpen ? 'max-h-[800px] opacity-100' : 'max-h-0 opacity-0'
+      }`}
+    >
+      <td colSpan={6} className='p-0 bg-transparent relative'>
+        {isOpen && (
+          <>
+            <Icon
+              icon='ph:handshake-thin'
+              width={250}
+              className='absolute inset-0 mx-auto opacity-10 dark:opacity-20 z-0'
+            />
             <table className='w-full'>
-              <tbody className='w-full bg-white-100 dark:bg-[#a9ba9d] relative'>
+              <thead className='text-white-100 bg-zinc-800'>
                 <tr>
-                  <div className='absolute inset-0 flex items-center justify-center opacity-10 dark:opacity-20 z-0'>
-                    <Icon icon='ph:handshake-thin' width={250} />
-                  </div>
-                  <table className='w-full'>
-                    <thead className='text-white-100 bg-zinc-800'>
-                      <tr>
-                        <td align='center'>Description</td>
-                        <td align='center'>Nr. Units </td>
-                        <td align='center'></td>
-                        <td align='center'>Unit Cost</td>
-                        <td align='center'>Total Cost</td>
-                      </tr>
-                    </thead>
-                    <tbody className='text-[#000]'>
-                      {typeOfMeetingProp === 'Full Day Meeting' ? (
-                        <>
-                          <MeetingBreakdownRow
-                            units={1}
-                            title='Full Day Rental Rate'
-                            rate={meeting?.FDRate || 0}
-                          />
-                          <MeetingBreakdownRow
-                            units={pax}
-                            title='Full Day Delegate Rate'
-                            rate={meeting?.FDDDR || 0}
-                          />
-                        </>
-                      ) : (
-                        <>
-                          <MeetingBreakdownRow
-                            units={1}
-                            title='Half Day Rental Rate'
-                            rate={meeting?.HDRate || 0}
-                          />
-                          <MeetingBreakdownRow
-                            units={pax}
-                            title='Half Day Delegate Rate'
-                            rate={meeting?.HDDDR || 0}
-                          />
-                        </>
-                      )}
-
-                      <MeetingBreakdownRow
-                        units={meeting?.coffeeBreakUnits || 0}
-                        title='Coffee Breaks'
-                        rate={meeting?.coffeeBreakPrice || 0}
-                      />
-                      <MeetingBreakdownRow
-                        units={meeting?.workingLunchUnits || 0}
-                        title='Working Lunch'
-                        rate={meeting?.workingLunchPrice || 0}
-                      />
-                      <MeetingBreakdownRow
-                        units={meeting?.hotelDinnerUnits || 0}
-                        title='Dinner @ Hotel'
-                        rate={meeting?.hotelDinnerPrice || 0}
-                      />
-                      <MeetingBreakdownRow
-                        units={1}
-                        title='Audio Visuals Package'
-                        rate={meeting?.aavvPackage || 0}
-                      />
-                    </tbody>
-                  </table>
+                  <th align='center'>Description</th>
+                  <th align='center'>Nr. Units</th>
+                  <th align='center'></th>
+                  <th align='center'>Unit Cost</th>
+                  <th align='center'>Total Cost</th>
                 </tr>
+              </thead>
+              <tbody className='text-[#000] bg-white-100 dark:bg-[#a9ba9d]'>
+                {typeOfMeetingProp === 'Full Day Meeting' ? (
+                  <>
+                    <MeetingBreakdownRow
+                      units={1}
+                      title='Full Day Rental Rate'
+                      rate={meeting?.FDRate || 0}
+                    />
+                    <MeetingBreakdownRow
+                      units={pax}
+                      title='Full Day Delegate Rate'
+                      rate={meeting?.FDDDR || 0}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <MeetingBreakdownRow
+                      units={1}
+                      title='Half Day Rental Rate'
+                      rate={meeting?.HDRate || 0}
+                    />
+                    <MeetingBreakdownRow
+                      units={pax}
+                      title='Half Day Delegate Rate'
+                      rate={meeting?.HDDDR || 0}
+                    />
+                  </>
+                )}
+                <MeetingBreakdownRow
+                  units={meeting?.coffeeBreakUnits || 0}
+                  title='Coffee Breaks'
+                  rate={meeting?.coffeeBreakPrice || 0}
+                />
+                <MeetingBreakdownRow
+                  units={meeting?.workingLunchUnits || 0}
+                  title='Working Lunch'
+                  rate={meeting?.workingLunchPrice || 0}
+                />
+                <MeetingBreakdownRow
+                  units={meeting?.hotelDinnerUnits || 0}
+                  title='Dinner @ Hotel'
+                  rate={meeting?.hotelDinnerPrice || 0}
+                />
+                <MeetingBreakdownRow
+                  units={1}
+                  title='Audio Visuals Package'
+                  rate={meeting?.aavvPackage || 0}
+                />
               </tbody>
             </table>
-          </div>
-        </td>
-      </tr>
-    </>
+          </>
+        )}
+      </td>
+    </tr>
   )
 }
--- src/hooks/useGetShowCost.ts
+++ src/hooks/useGetShowCost.ts
@@ -1,4 +1,26 @@
+import { useBudget } from './useBudget'
+
+interface ShowDetails {
+  showCost?: number
+}
+
+interface DayDetails {
+  lunch?: ShowDetails
+  dinner?: ShowDetails
+}
+
 export const useGetShowCost = () => {
-  const showTotalCost = 450
+  const { shows } = useBudget()
+  let showTotalCost = 0
+
+  Object.values(shows).forEach((day: DayDetails) => {
+    if (day.lunch?.showCost) {
+      showTotalCost += day.lunch.showCost
+    }
+    if (day.dinner?.showCost) {
+      showTotalCost += day.dinner.showCost
+    }
+  })
+
   return { showTotalCost }
 }
